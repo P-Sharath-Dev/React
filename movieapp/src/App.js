64009baea86890af3e90a3ec4280e6522d8d72f0
Sharath <sharath03.tech@gmail.com>
2026-06-01 08:41:03 +0530

    learned how to use class-based component, setState()

---
+++ movieapp/src/App.js
@@ -0,0 +1,12 @@
+// import "./App.css";
+import MovieCard from "./MovieCard";
+
+function App() {
+  return (
+    <div className="App">
+      <MovieCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ movieapp/src/MovieCard.js
@@ -0,0 +1,103 @@
+//implementing class components
+import { Component } from "react";
+
+export default class MovieCard extends Component {
+  //creating state object
+  constructor() {
+    super();
+    this.state = {
+      title: "SPIDER-MAN : BRAND NEW DAY",
+      plot: "Alone and crime-fighting in a New York City that no longer knows his name, Peter Parker has devoted himself to being a full-time Spider-Man. But as the pressure on Spider-Man intensifies, Peter undergoes a surprising physical evolution that threatens his existence.",
+      price: 199,
+      poster:
+        "https://cdn.marvel.com/content/1x/spidermanbrandnewday_online_1080x1350_ninjas_02.jpg",
+      rating: 9.5,
+      star: 0,
+    };
+    this.addStar = this.addStar.bind(this);
+    // this.decreaseStar = this.decreaseStar.bind(this);
+  }
+
+  //normal function for increasing the star
+  // addStar() {
+  //   console.log("star added");
+  // }
+
+  //arrow function instead of bind method
+  addStar = () => {
+    // console.log("added star ");
+    // console.log(" before :: this.state ", this.state);
+
+    // this.state.star += 0.5;
+
+    // update star till 5 only
+    if (this.state.star < 5) {
+      //
+      //1st way of setting state using setState()
+      //
+      // this.setState({
+      //   //react provieds setState
+      //   star: this.state.star + 0.5,
+      // });
+      //
+      //2nd way of update using setState();
+      this.setState((prevState) => {
+        return {
+          star: prevState.star + 0.5,
+        };
+      });
+    }
+    // console.log(" after :: this.state ", this.state);
+  };
+
+  //function for decreasing star
+  decreaseStar = () => {
+    if (this.state.star > 0) {
+      this.setState((prevState) => {
+        return {
+          star: prevState.star - 0.5,
+        };
+      });
+    }
+  };
+
+  render() {
+    const { title, plot, price, poster, rating, star } = this.state;
+    return (
+      <div className="main">
+        <div className="movie-card">
+          {/* moviecard's left side */}
+          <div className="left">
+            <img src={poster} alt="Poster" />
+          </div>
+          {/* moviecard's right side */}
+          <div className="right">
+            <div className=" title">{title}</div>
+            <div className="plot">{plot}</div>
+            <div className="price">Rs.{price}</div>
+            {/* footer */}
+            <div className="footer">
+              <div className="rating">{rating}</div>
+              <div className="stars">
+                <i
+                  className="fa-regular fa-square-plus"
+                  onClick={this.addStar}
+                ></i>
+                &nbsp;
+                <i
+                  className="fa-regular fa-square-minus"
+                  onClick={this.decreaseStar}
+                ></i>
+                &nbsp;
+                <i className="fas fa-star"></i>
+                <span className="star-count">{star}</span>
+              </div>
+              <button className="favourite-btn">Favourite</button>
+              <button className="cart-btn">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
