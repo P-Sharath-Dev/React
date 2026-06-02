fcc2e4688c8495fb00e1c04af0fbad85d44f8cb3
Sharath <sharath03.tech@gmail.com>
2026-06-02 13:18:08 +0530

    moved functions from movieCard to movieList

--- movieapp/src/MovieCard.js
+++ movieapp/src/MovieCard.js
@@ -2,73 +2,8 @@
 import { Component } from "react";
 
 export default class MovieCard extends Component {
-  //creating state object
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     title: "SPIDER-MAN : BRAND NEW DAY",
-  //     plot: "Alone and crime-fighting in a New York City that no longer knows his name, Peter Parker has devoted himself to being a full-time Spider-Man. But as the pressure on Spider-Man intensifies, Peter undergoes a surprising physical evolution that threatens his existence.",
-  //     price: 199,
-  //     poster:
-  //       "https://cdn.marvel.com/content/1x/spidermanbrandnewday_online_1080x1350_ninjas_02.jpg",
-  //     rating: 9.5,
-  //     star: 0,
-  //     isFavourte: false,
-  //     isAddedToCart: false,
-  //   };
-  //   this.addStar = this.addStar.bind(this);
-  //   // this.decreaseStar = this.decreaseStar.bind(this);
-  // }
-
-  //arrow function instead of bind method
-  addStar = () => {
-    // update star till 5 only
-    if (this.props.star < 5) {
-      //2nd way of update using setState(); (passing callback in setState())
-      this.setState(
-        (prevState) => {
-          return {
-            star: prevState.star + 0.5,
-          };
-        },
-        () => {
-          console.log("star in setState : ", this.props.star);
-        },
-      );
-    }
-  };
-
-  //function for decreasing star
-  decreaseStar = () => {
-    if (this.props.star > 0) {
-      this.setState((prevState) => {
-        return {
-          star: prevState.star - 0.5,
-        };
-      });
-    }
-  };
-
-  //handling fav button
-  handleFavourite = () => {
-    this.setState({
-      isFavourte: !this.props.isFavourte,
-    });
-  };
-
-  //handling cart button
-  handleCart = () => {
-    this.setState(
-      {
-        isAddedToCart: !this.props.isAddedToCart,
-      },
-      // () => {
-      //   console.log("isFavourite : ", this.state.isFavourte);
-      // },
-    );
-  };
-
   render() {
+    // Getting movie data from props
     const {
       title,
       plot,
@@ -80,6 +15,13 @@ export default class MovieCard extends Component {
       isAddedToCart,
     } = this.props.movie;
 
+    // Getting functions from parent component through props
+    const {
+      handleFavourite,
+      handleIncreseStar,
+      handleDecreaseStar,
+      handleCart,
+    } = this.props;
     return (
       <div className="main">
         <div className="movie-card">
@@ -87,41 +29,49 @@ export default class MovieCard extends Component {
           <div className="left">
             <img src={poster} alt="Poster" />
           </div>
+
           {/* moviecard's right side */}
           <div className="right">
             <div className=" title">{title}</div>
             <div className="plot">{plot}</div>
             <div className="price">Rs.{price}</div>
+
             {/* footer */}
             <div className="footer">
+              {/* Movie rating */}
               <div className="rating">{rating}</div>
+              {/* Increase and decrease star buttons */}
               <div className="stars">
+                {/* Increase star */}
                 <i
                   className="fa-regular fa-square-plus"
-                  onClick={this.addStar}
+                  onClick={() => handleIncreseStar(this.props.movie)}
                 ></i>
-                &nbsp;
+                {/* Decrease star */}
                 <i
                   className="fa-regular fa-square-minus"
-                  onClick={this.decreaseStar}
+                  onClick={() => handleDecreaseStar(this.props.movie)}
                 ></i>
-                &nbsp;
+
                 <i className="fas fa-star"></i>
+                {/* Current star value */}
                 <span className="star-count">{star}</span>
               </div>
 
               {/* conditional rendering for favourite, UnFavourite buttons */}
 
+              {/* Favourite / Unfavourite button */}
               <button
                 className={isFavourte ? "unfavourite-btn" : "favourite-btn"}
-                onClick={this.handleFavourite}
+                onClick={() => handleFavourite(this.props.movie)}
               >
                 {isFavourte ? "Unfavourite" : "favourite"}
               </button>
 
+              {/* Add to cart / Remove from cart button */}
               <button
                 className={isAddedToCart ? "addCart-btn" : "removeCart-btn"}
-                onClick={this.handleCart}
+                onClick={() => handleCart(this.props.movie)}
               >
                 {isAddedToCart ? "Remove from cart" : "Add to cart"}
               </button>
--- movieapp/src/MovieList.js
+++ movieapp/src/MovieList.js
@@ -5,6 +5,7 @@ export default class MovieList extends Component {
   constructor() {
     super();
     this.state = {
+      // Movies stored in component state
       movies: [
         {
           title: "SPIDER-MAN : BRAND NEW DAY",
@@ -21,8 +22,7 @@ export default class MovieList extends Component {
           title: "Intersteller",
           plot: "Alone and crime-fighting in a New York City that no longer knows his name, Peter Parker has devoted himself to being a full-time Spider-Man. But as the pressure on Spider-Man intensifies, Peter undergoes a surprising physical evolution that threatens his existence.",
           price: 299,
-          poster:
-            "https://cdn.marvel.com/content/1x/spidermanbrandnewday_online_1080x1350_ninjas_02.jpg",
+          poster: "https://m.media-amazon.com/images/I/713qGg4kCzL.jpg",
           rating: 9.5,
           star: 4.5,
           isFavourte: false,
@@ -33,7 +33,7 @@ export default class MovieList extends Component {
           plot: "Alone and crime-fighting in a New York City that no longer knows his name, Peter Parker has devoted himself to being a full-time Spider-Man. But as the pressure on Spider-Man intensifies, Peter undergoes a surprising physical evolution that threatens his existence.",
           price: 89,
           poster:
-            "https://cdn.marvel.com/content/1x/spidermanbrandnewday_online_1080x1350_ninjas_02.jpg",
+            "https://i.pinimg.com/736x/60/ca/54/60ca54ef5069ec9cc565dbb0d1b9fd11.jpg",
           rating: 7,
           star: 3,
           isFavourte: false,
@@ -43,29 +43,109 @@ export default class MovieList extends Component {
     };
   }
 
+  // Increase movie star count
+  addStar = (movie) => {
+    const { movies } = this.state;
+
+    // Find index of clicked movie
+    const movieIndex = movies.indexOf(movie);
+
+    // Increase star only if it is less than 5
+    if (movies[movieIndex].star < 5) {
+      movies[movieIndex].star += 0.5;
+
+      // Update state
+      this.setState({
+        movies: movies,
+      });
+      // console.log("after state update star : ", movies[movieIndex].star);
+    }
+  };
+
+  // Decrease movie star count
+  decreaseStar = (movie) => {
+    const { movies } = this.state;
+
+    // Find index of clicked movie
+    const movieIndex = movies.indexOf(movie);
+
+    // Decrease star only if it is greater than 0
+    if (movies[movieIndex].star > 0) {
+      movies[movieIndex].star -= 0.5;
+
+      // Update state
+      this.setState({
+        movies: movies,
+      });
+      // console.log("after state update star : ", movies[movieIndex].star);
+    }
+  };
+
+  // Add or remove movie from cart
+  handleCart = (movie) => {
+    const { movies } = this.state;
+    const movieIndex = movies.indexOf(movie);
+
+    // Toggle cart value
+    movies[movieIndex].isAddedToCart = !movies[movieIndex].isAddedToCart;
+
+    //update state
+    this.setState(
+      {
+        movies: movies,
+      },
+      () => {
+        // console.log(
+        //   "movies[movieIndex].isAddedToCart : ",
+        //   movies[movieIndex].isAddedToCart,
+        // );
+      },
+    );
+  };
+
+  // Mark movie as favourite or unfavourite
+  handleFavourite = (movie) => {
+    const { movies } = this.state;
+    const movieIndex = movies.indexOf(movie);
+
+    // Toggle favourite value
+    movies[movieIndex].isFavourte = !movies[movieIndex].isFavourte;
+    this.setState(
+      {
+        movies: movies,
+      },
+      () => {
+        // console.log(
+        //   "movies[movieIndex].isFavourte : ",
+        //   movies[movieIndex].isFavourte,
+        // );
+      },
+    );
+  };
+
   render() {
     return (
       <>
-        {/* //using props
-        <MovieCard
-          title={this.state.title}
-          plot={this.state.plot}
-          price={this.state.price}
-          poster={this.state.poster}
-          rating={this.state.rating}
-          star={this.state.star}
-          isFavourte={this.state.isFavourte}
-          isAddedToCart={this.state.isAddedToCart}
-        /> */}
+        {/* Render all movie cards */}
         {this.state.movies.map((movie, index) => {
-          return <MovieCard key={index} movie={movie} />;
+          // Pass movie data and functions to MovieCard
+          return (
+            <MovieCard
+              key={index}
+              movie={movie}
+              handleIncreseStar={this.addStar}
+              handleDecreaseStar={this.decreaseStar}
+              handleFavourite={this.handleFavourite}
+              handleCart={this.handleCart}
+            />
+          );
         })}
       </>
     );
   }
 }
 
-// Put defaultProps here
+// Default values if movie data is not passed
 MovieCard.defaultProps = {
   movie: {
     title: "No Title",
