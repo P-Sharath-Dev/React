75d6edf87dc18984073dd04b6ba79742de4a2f41
Sharath <sharath03.tech@gmail.com>
2026-06-08 16:44:15 +0530

    ..

---
+++ lifecycle/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import ComponentA from "./ComponentA";
+
+class App extends Component {
+  render() {
+    return <ComponentA />;
+  }
+}
+
+export default App;
---
+++ lifecycle/src/ComponentA.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+export default class ComponentA extends Component {
+  //constructor() is called first implicitly no matter where you put it.(not only in react. in general this is how class works)
+  constructor() {
+    super();
+    this.state = {
+      name: "learning lifecycle methods",
+    };
+
+    this.setState({
+      name: "steve",
+    });
+    console.log("constructor from component A");
+  }
+
+  //rencer
+  render() {
+    console.log("render method from component A");
+    return <h1>Component A : {this.state.name}</h1>;
+  }
+
+  //getDerivedStateFromProps function
+  static getDerivedStateFromProps() {
+    console.log("from getDerivedStateFromProps()");
+    return null;
+  }
+
+  //componentDidMount
+  componentDidMount() {
+    console.log("from componentDidMount");
+  }
+}
